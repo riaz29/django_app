bd7b0c52650c1a797a7ca576274d3c74709c2038
Sheikh Riaz-ul-Haq Qureshi <riaz.haq@dolmengroup.com>
2024-02-29 14:55:51 +0500

    Dynamic URl with type with Dict

--- app1/views.py
+++ app1/views.py
@@ -3,6 +3,15 @@ from django.http import HttpRequest
 from django.http import HttpResponse ,HttpResponseNotFound
 # Create your views here.
 
+task_dict ={
+    "task1":"This is task1",
+    "task2":"This is task2",
+    "task3":"This is task3",
+    "task4":"This is task4",
+    "task5":"This is task5",
+    "task6":"This is task6",
+}
+
 # def task1(request):
 #     return HttpResponse("This is task 1")
 
@@ -13,14 +22,21 @@ def tasks_int(request, task):
     return HttpResponse(task)
 
 def tasks(request, task):
-    task_text = None
-    if task == "task1":
-        task_text = "This is task1"
-    elif task == "task2":
-        task_text = "This is task2"
-    elif task == "task3":
-        task_text = "This is task3"
-    else:
-        return HttpResponseNotFound("This Task is not availible")
+    try:
+        task_text = task_dict[task]
+        return HttpResponse(task_text)
+    except:
+        return HttpResponseNotFound("Invalid Task")
+        
+# def tasks(request, task):
+#     task_text = None
+#     if task == "task1":
+#         task_text = "This is task1"
+#     elif task == "task2":
+#         task_text = "This is task2"
+#     elif task == "task3":
+#         task_text = "This is task3"
+#     else:
+#         return HttpResponseNotFound("This Task is not availible")
     
-    return HttpResponse(task_text)
+#     return HttpResponse(task_text)
