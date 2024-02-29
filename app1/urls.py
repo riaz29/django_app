2259822b16af2b4c6d549e16c3871a7a18682531
Sheikh Riaz-ul-Haq Qureshi <riaz.haq@dolmengroup.com>
2024-02-29 11:26:50 +0500

    Dynamic URl

--- app1/urls.py
+++ app1/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("task1", views.task1),
-    path("task2", views.task2)
+    # path("task1", views.task1),
+    # path("task2", views.task2)
+    path("<task>", views.tasks)
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-from django.http import HttpResponse
+from django.http import HttpResponse ,HttpResponseNotFound
 # Create your views here.
 
-def task1(request):
-    return HttpResponse("This is task 1")
+# def task1(request):
+#     return HttpResponse("This is task 1")
 
-def task2(request):
-    return HttpResponse("This is task 2")
+# def task2(request):
+#     return HttpResponse("This is task 2")
+
+def tasks(request, task):
+    task_text = None
+    if task == "task1":
+        task_text = "This is task1"
+    elif task == "task2":
+        task_text = "This is task2"
+    elif task == "task3":
+        task_text = "This is task3"
+    else:
+        return HttpResponseNotFound("This Task is not availible")
+    
+    return HttpResponse(task_text)
