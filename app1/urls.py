a48bea443da1708ec1ba63861fa2e7ce4b91a569
Sheikh Riaz-ul-Haq Qureshi <riaz.haq@dolmengroup.com>
2024-03-19 10:09:56 +0500

    Includes Things 4

--- app1/urls.py
+++ app1/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index , name="index"),
     path("<int:task>", views.tasks_int),
     path("<str:task>", views.tasks, name="task-name")
     
